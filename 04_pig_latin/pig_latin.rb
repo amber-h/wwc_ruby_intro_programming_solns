1b1f330d7c0615bf21c7925a7b99627665c62a5a
amber <amber.houle3@gmail.com>
2015-02-06 01:26:00 -0600

    adding pig latin soln

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,3 +1,11 @@
 def translate(string)
+     vowels = ["a","e","i","o","u"]
 
+     if vowels.include?(string[0])string
+        string.concat("ay")
+     elsif !vowels.include?(string[0]) && vowels.include?(string[1])
+       string.gsub(string[0,1],"").concat(string[0,1]).concat("ay")
+     elsif !vowels.include?(string[0,2])
+       string.gsub(string[0,2],"").concat(string[0,2]).concat("ay")
+        end
 end
