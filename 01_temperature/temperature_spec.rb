850926adb1608ff351b0948240893cddfc329efb
amber <amber.houle3@gmail.com>
2015-02-05 23:20:46 -0600

    Amber - added temperature.rb class and renamed farenheit and celsius methods in temperature_spec.rb

--- 01_temperature/temperature_spec.rb
+++ 01_temperature/temperature_spec.rb
@@ -1,7 +1,7 @@
 # # Topics:
 # * functions
 # * floating-point math
-#
+# * arithmetic functions
 # # Hints
 #
 # Remember that one degree fahrenheit is 5/9 of one degree celsius, and that the freezing point of water is 0 degrees celsius but 32 degrees fahrenheit.
@@ -19,42 +19,42 @@ require "temperature"
 
 describe "temperature conversion functions" do
 
-  describe "#ftoc" do
+  describe "#farenheit_to_celsius" do
 
     it "converts freezing temperature" do
-      ftoc(32).should == 0
+      farenheit_to_celsius(32).should == 0
     end
 
     it "converts boiling temperature" do
-      ftoc(212).should == 100
+      farenheit_to_celsius(212).should == 100
     end
 
     it "converts body temperature" do
-      ftoc(98.6).should == 37
+      farenheit_to_celsius(98.6).should == 37
     end
 
     it "converts arbitrary temperature" do
-      ftoc(68).should == 20
+      farenheit_to_celsius(68).should == 20
     end
 
   end
 
-  describe "#ctof" do
+  describe "#celsius_to_farenheit" do
 
     it "converts freezing temperature" do
-      ctof(0).should == 32
+      celsius_to_farenheit(0).should == 32
     end
 
     it "converts boiling temperature" do
-      ctof(100).should == 212
+      celsius_to_farenheit(100).should == 212
     end
 
     it "converts arbitrary temperature" do
-      ctof(20).should == 68
+      celsius_to_farenheit(20).should == 68
     end
 
     it "converts body temperature" do
-      ctof(37).should be_within(0.1).of(98.6)
+      celsius_to_farenheit(37).should be_within(0.1).of(98.6)
       # Why do we need to use be_within?
       # See http://www.ruby-forum.com/topic/169330
       # and http://en.wikipedia.org/wiki/IEEE_754-2008
